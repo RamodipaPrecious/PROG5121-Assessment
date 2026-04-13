b7937d1d9c2168345b72f7a6e71bb1513ac342a1
Ramodipa Precious <preciousramodipa19@gmail.com>
2026-04-13 21:40:55 +0200

    cherk username

--- POE/src/poe/Login.java
+++ POE/src/poe/Login.java
@@ -18,6 +18,20 @@ public class Login {
         this.password = password;
         this.UserList = new ArrayList<>();
     }
-}
     
-    
+    public boolean checkUserName()
+    {
+        int tempNameLength = this.Usersname.length(); //The length of the username
+        
+        if(tempNameLength <= 5 &&this.Usersname.contains("_") ==true)
+        {
+            System.out.println("Username  successfully captured.");
+            return true; //Conditions been met.
+        }else
+        {
+            System.err.println("Username is not correctly formatted; please ensure that "
+                    + "your username contains an underscore and is no more than five characters in length.");
+            return false; //not met the min requirements
+        }
+    }
+   
